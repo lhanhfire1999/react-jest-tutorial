329de25c2051c80e41607156e27f1831fa6e9779
Anh Le <lhanhfire1999@gmail.com>
2022-12-04 02:22:54 +0700

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,23 @@
+import logo from './logo.svg'
 import './App.css'
-import { useState } from 'react'
-
-export const handleReplaceCamelCaseWithSpace = (colorName) => {
-  return colorName.replace(/\B([A-Z])\B/g, ' $1')
-}
 
 function App() {
-  const [colorToggle, setColorToggle] = useState('MediumVioletRed')
-  const [isCheckbox, setIsCheckbox] = useState(false)
-  const newButtonColor =
-    colorToggle === 'MediumVioletRed' ? 'MidnightBlue' : 'MediumVioletRed'
-
-  const handleChangeColor = () => {
-    setColorToggle((prev) =>
-      prev === 'MediumVioletRed' ? 'MidnightBlue' : 'MediumVioletRed'
-    )
-  }
-
-  const handleChangeCheckbox = () => {
-    setIsCheckbox((prev) => !prev)
-  }
   return (
-    <div>
-      <button
-        onClick={handleChangeColor}
-        style={{ backgroundColor: `${isCheckbox ? 'gray' : colorToggle}` }}
-        disabled={isCheckbox}
-      >
-        Change to {handleReplaceCamelCaseWithSpace(newButtonColor)}
-      </button>
-      <input
-        id="disabled-button-checkbox"
-        type="checkbox"
-        value={isCheckbox}
-        onChange={handleChangeCheckbox}
-      />
-
-      <label htmlFor="disabled-button-checkbox">Disabled button</label>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   )
 }
