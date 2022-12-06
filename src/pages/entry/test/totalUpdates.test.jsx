f0c0727348ff3a88d3b2c4fcadc2bf493a80c0b3
Anh Le <lhanhfire1999@gmail.com>
2022-12-07 03:00:17 +0700

    Test and code topping subtotal

--- src/pages/entry/test/totalUpdates.test.jsx
+++ src/pages/entry/test/totalUpdates.test.jsx
@@ -3,7 +3,7 @@ import userEvent from '@testing-library/user-event'
 
 import { render, screen } from '../../../test-utils/test-library-utils'
 
-test('Update subtotal when scoops change', async () => {
+test('Update scoop subtotal when scoops change', async () => {
   render(<Options optionType="scoops" />)
 
   const scoopTotal = screen.getByText('Scoops total: $', { exact: false })
@@ -22,4 +22,26 @@ test('Update subtotal when scoops change', async () => {
   await userEvent.clear(chocolateInput)
   await userEvent.type(chocolateInput, '2')
   expect(scoopTotal).toHaveTextContent('6.00')
+})
+
+test('Update topping subtotal when topping change', async () => {
+  render(<Options optionType="toppings" />)
+  const toppingTotal = screen.getByText(`Toppings total: $`, { exact: false })
+  expect(toppingTotal).toHaveTextContent('0.00')
+
+  const cherriesCheckbox = await screen.findByRole('checkbox', {
+    name: 'Cherries',
+  })
+
+  await userEvent.click(cherriesCheckbox)
+  expect(toppingTotal).toHaveTextContent('1.50')
+
+  const hotFudgeCheckbox = await screen.findByRole('checkbox', {
+    name: 'Hot fudge',
+  })
+  await userEvent.click(hotFudgeCheckbox)
+  expect(toppingTotal).toHaveTextContent('3.00')
+
+  await userEvent.click(hotFudgeCheckbox)
+  expect(toppingTotal).toHaveTextContent('1.50')
 })
