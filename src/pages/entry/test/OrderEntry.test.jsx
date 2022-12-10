9900bb771cd1d78b0057159f2a3341d2dcff1db0
Anh Le <lhanhfire1999@gmail.com>
2022-12-11 02:31:20 +0700

    Test confirm button on orderEntry page

--- src/pages/entry/test/OrderEntry.test.jsx
+++ src/pages/entry/test/OrderEntry.test.jsx
@@ -2,6 +2,7 @@ import { render, screen, waitFor } from '../../../test-utils/test-library-utils'
 import { rest } from 'msw'
 import { server } from '../../../mocks/server'
 import OrderEntry from '../OrderEntry'
+import useEvent from '@testing-library/user-event'
 
 test('handles error for scoop and topping routes', async () => {
   server.use(
@@ -15,8 +16,27 @@ test('handles error for scoop and topping routes', async () => {
 
   render(<OrderEntry />)
 
-  await waitFor(async () => {
-    const alerts = await screen.findAllByRole('alert')
-    expect(alerts).toHaveLength(2)
+  // await waitFor(async () => {
+  const alerts = await screen.findAllByRole('alert')
+  expect(alerts).toHaveLength(2)
+  // })
+})
+
+test('Enabled button when increase quantity the particular scoop to 1, and disabled button when decrease it to 0', async () => {
+  const user = useEvent.setup()
+  render(<OrderEntry />)
+
+  const chocolateInput = await screen.findByRole('spinbutton', {
+    name: /chocolate/i,
   })
+  await user.clear(chocolateInput)
+  await user.type(chocolateInput, '1')
+
+  const orderButton = screen.getByRole('button', { name: /order ice cream/i })
+  expect(orderButton).toBeEnabled()
+
+  await user.clear(chocolateInput)
+  await user.type(chocolateInput, '0')
+
+  expect(orderButton).toBeDisabled()
 })
